a9626927f307419823673f6484e3c8ad6256f30d
juliushekkala <julius.hekkala@gmail.com>
2020-02-07 15:45:31 +0200

    ORM model stubs

---
+++ src/orm_models.py
@@ -0,0 +1,20 @@
+class Game(db.Model):
+
+
+
+class Player(db.Model):
+
+
+class PlayerScore(db.Model):
+
+
+class GameType(db.Model):
+
+
+class Leaderboard(db.Model):
+
+
+class Permission(db.Model):
+
+
+class Tournament(db.Model):
