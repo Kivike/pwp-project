616a1d7033c48285f5ac554cfd59a704b2c453bd
Kivike <coloris23@gmail.com>
2020-03-10 21:53:22 +0200

    Add additional routes for leaderboard

--- src/router.py
+++ src/router.py
@@ -20,14 +20,22 @@ def route_app(app):
     api.add_resource(Gametype, "/api/gametypes/<gametype_name>/")
     api.add_resource(GametypeCollection, "/api/gametypes/")
 
-    api.add_resource(LeaderboardGametype, "/api/leaderboard/gametype/<gametype_name>/")
-    api.add_resource(LeaderboardPlayer, "/api/leaderboard/player/<player_name>/")
+    api.add_resource(
+        LeaderboardGametype,
+        "/api/leaderboard/gametype/<gametype_name>/",
+        "/api/gametype/<gametype_name>/leaderboard/"
+    )
+    api.add_resource(
+        LeaderboardPlayer,
+        "/api/leaderboard/player/<player_name>/",
+        "/api/players/<player_name>/leaderboard/"
+    )
 
     api.add_resource(Player, "/api/players/<player_name>/")
     api.add_resource(PlayerCollection, "/api/players/")
 
-    api.add_resource(GameScoreboard, "/api/games/<game_token>/scoreboard")
-    api.add_resource(PlayerScore, "/api/games/<game_token>/scoreboard/<player_name>")
+    api.add_resource(GameScoreboard, "/api/games/<game_token>/scoreboard/")
+    api.add_resource(PlayerScore, "/api/games/<game_token>/scoreboard/<player_name>/")
 
     api.add_resource(Tournament, "/api/tournaments/<tournament_token>/")
     api.add_resource(TournamentCollection, "/api/tournaments/")
