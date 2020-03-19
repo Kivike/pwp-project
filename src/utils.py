8caf97364fe8dcd126f831bbcce2c1391cea98db
juliushekkala <julius.hekkala@gmail.com>
2020-03-19 18:46:33 +0200

    Controls for Player and PlayerCollection in utils
    Also added Player schema builder to utils.

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,11 @@
 import json
 
-from flask import request, Response
+from flask import request, Response, url_for
+
+
 
 MASON = "application/vnd.mason+json"
+
 
 class MasonBuilder(dict):
     """
@@ -69,8 +72,42 @@ class MasonBuilder(dict):
         self["@controls"][ctrl_name]["href"] = href
 
 class PlayerBuilder(MasonBuilder):
-    #TODO
-    pass
+    
+    @staticmethod
+    def playerSchema():
+        schema = {
+            "type": "object",
+            "required": ["name"]
+        }
+        props = schema["properties"] = {}
+        props["name"] = {
+            "description": "Name of the player",
+            "type": "string"
+        } 
+        return schema
+
+    #Controls related to Player and PlayerCollection
+
+    def add_control_all_games(self):
+        self.add_control("gamescr:all-games", href=url_for("gamecollection"), title="All games")
+
+    def add_control_all_tournaments(self):
+        self.add_control("gamescr:all-tournaments", href=url_for("tournamentcollection"), title="All tournaments")
+
+    def add_control_add_player(self):
+        schema = self.playerSchema()
+        self.add_control(ctrl_name="gamescr:add-player", href=url_for("playercollection"), method="POST", encoding="json", schema=schema, title="Add a mew player")
+
+    def add_control_edit_player(self, name):
+        schema = self.playerSchema()
+        self.add_control(ctrl_name="edit", href=url_for("player", player_name=name), method="PUT", encoding="json", schema=schema, title="Modify the player")
+    
+    def add_control_delete_player(self, name):
+        self.add_control(ctrl_name="gamescr:delete", href=url_for("player", player_name=name), method="DELETE", title="Delete this player")
+    
+    def add_control_leaderboard(self, name):
+        self.add_control(ctrl_name="gamescr:leaderboard", href=url_for("leaderboardplayer", player_name=name), title="Statistics of this player")
+
 
 def create_error_response(status_code, title, message=None):
     resource_url = request.path
