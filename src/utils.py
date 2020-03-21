23ad4e6efe843c94e117ba5212d60e757f71100a
Otto Poikajärvi <tootsi666@gmail.com>
2020-03-21 16:45:13 +0200

    Added GameBuilder and GametypeBuilder

--- src/utils.py
+++ src/utils.py
@@ -96,7 +96,7 @@ class PlayerBuilder(MasonBuilder):
 
     def add_control_add_player(self):
         schema = self.playerSchema()
-        self.add_control(ctrl_name="gamescr:add-player", href=url_for("playercollection"), method="POST", encoding="json", schema=schema, title="Add a mew player")
+        self.add_control(ctrl_name="gamescr:add-player", href=url_for("playercollection"), method="POST", encoding="json", schema=schema, title="Add a new player")
 
     def add_control_edit_player(self, name):
         schema = self.playerSchema()
@@ -107,6 +107,138 @@ class PlayerBuilder(MasonBuilder):
     
     def add_control_leaderboard(self, name):
         self.add_control(ctrl_name="gamescr:leaderboard", href=url_for("leaderboardplayer", player_name=name), title="Statistics of this player")
+
+class GameBuilder(MasonBuilder):
+
+    @staticmethod
+    def gameSchema():
+        schema = {
+            "type": "object",
+            "required": ["name", "game_type", "host"]
+        }
+        props = schema["properties"] = {}
+        props["name"] = {
+            "description": "Name used to access the game",
+            "type": "string"
+        }
+        props["status"] = {
+            "description": "1 if active, 0 if not active",
+            "type": "integer",
+            "default": 0
+        }
+        props["game_type"] = {
+            "description": "Name of the game type",
+            "type": "string"
+        }
+        props["host"] = {
+            "description": "Name of the player hosting the game",
+            "type": "string"
+        }
+        props["tournament"] = {
+            "description": "Name of the tournament the game is a part of",
+            "type": "string"
+        }
+        return schema
+
+    @staticmethod
+    def scoreSchema():
+        schema = {
+            "type": "object",
+            "required": ["player", "game", "score"]
+        }
+        props = schema["properties"] = {}
+        props["player"] = {
+            "description": "Name of the player",
+            "type": "string"
+        }
+        props["game"] = {
+            "description": "Access name of the game",
+            "type": "string"
+        }
+        props["score"] = {
+            "description": "Score of the player",
+            "type": "float"
+        }
+        return schema
+
+    # Controls for Game and GameCollection
+
+    def add_control_all_tournaments(self):
+        self.add_control("gamescr:all-tournaments", href=url_for("tournamentcollection"), title="All tournaments")
+
+    def add_control_all_gametypes(self):
+        self.add_control("gamescr:all-gametypes", href=url_for("gametypecollection"), title="All gametypes")
+
+    def add_control_all_players(self):
+        self.add_control("gamescr:all-players", href=url_for("playercollection"), title="All players")
+
+    def add_control_add_game(self):
+        schema = self.gameSchema()
+        self.add_control(ctrl_name="gamescr:add-game", href=url_for("gamecollection"), method="POST", encoding="json", schema=schema, title="Add a new game")
+
+    def add_control_gametype(self, name):
+        self.add_control(ctrl_name="gamescr:gametype", href=url_for("gametyperesource", gametype_name=name), title="Gametype of this game")
+
+    def add_control_scores(self, name):
+        self.add_control(ctrl_name="gamescr:scores", href=url_for("gamescoreboard", game_token=name), title="Scores of players in this game")
+
+    def add_control_tournament(self, name):
+        self.add_control(ctrl_name="gamescr:tournament", href=url_for("tournamentresource", tournament_token=name), title="Tournament associated with this game")
+
+    def add_control_add_score(self, name):
+        schema = self.scoreSchema()
+        self.add_control(ctrl_name="gamescr:add-score", href=url_for("gamescoreboard", game_token=name), method="POST", encoding="json", schema=schema, title="Add a new player to the game")
+
+    def add_control_edit_game(self, name):
+        schema = self.gameSchema()
+        self.add_control(ctrl_name="edit", href=url_for("gameresource", game_token=name), method="PUT", encoding="json", schema=schema, title="Edit this game")
+
+    def add_control_delete_game(self, name):
+        self.add_control(ctrl_name="gamescr:delete", href=url_for("gameresource", game_token=name), method="DELETE", title="Delete this game")
+
+class GametypeBuilder(MasonBuilder):
+
+    @staticmethod
+    def gametypeSchema():
+        schema = {
+            "type": "object",
+            "required": ["name"]
+        }
+        props = schema["properties"] = {}
+        props["name"] = {
+            "description": "Name of the game type",
+            "type": "string"
+        }
+        props["max_players"] = {
+            "description": "Maximum amount of players allowed per game",
+            "type": "integer"
+        }
+        props["min_players"] = {
+            "description": "Minimum amount of players per game",
+            "type": "integer"
+        }
+        return schema
+
+    def add_control_all_games(self):
+        self.add_control("gamescr:all-games", href=url_for("gamecollection"), title="All games")
+
+    def add_control_add_gametype(self):
+        schema = self.gametypeSchema()
+        self.add_control(ctrl_name="gamescr:add-gametype", href=url_for("gametypecollection"), method="POST", encoding="json", schema=schema, title="Add a new gametype")
+
+    def add_control_edit_gametype(self, name):
+        schema = self.gametypeSchema()
+        self.add_control(ctrl_name="edit", href=url_for("gametyperesource", gametype_name=name), method="PUT", encoding="json", schema=schema, title="Edit this gametype")
+
+    def add_control_leaderboard(self, name):
+        self.add_control(ctrl_name="gamescr:leaderboard", href=url_for("leaderboardgametype", gametype_name=name), title="Leaderboard for this gametype")
+
+    def add_control_delete_gametype(self, name):
+        self.add_control(ctrl_name="gamescr:delete", href=url_for("gametyperesource", gametype_name=name), method="DELETE", title="Delete this gametype")
+
+class scoreBuilder(MasonBuilder):
+    #TODO
+    pass
 
 
 def create_error_response(status_code, title, message=None):
