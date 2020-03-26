f87af205830da4a559a4d1d710490e9f24a40fb4
juliushekkala <julius.hekkala@gmail.com>
2020-03-26 17:07:06 +0200

    Renamed game_token to game_name, imports

--- src/resource/game.py
+++ src/resource/game.py
@@ -1,4 +1,16 @@
-from flask_restful import Resource
+from flask_restful import Resource, url_for
+from flask import request, Response
+from sqlalchemy.exc import IntegrityError
+from src.utils import GameBuilder
+from src.utils import create_error_response
+from src.orm_models import Game
+from src.extensions import db
+import json
+from jsonschema import validate, ValidationError
+
+MASON = "application/vnd.mason+json"
+NAMESPACE_URL = "/gamescore/link-relations#"
+PROFILE_URL = "/profiles/game/"
 
 class GameCollection(Resource):
     def get(self):
