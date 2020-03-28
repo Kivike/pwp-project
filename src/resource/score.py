37084af5c51fa66d4b1040398dd891fc846da6ff
Otto Poikajärvi <tootsi666@gmail.com>
2020-03-28 15:13:08 +0200

    Imports

--- src/resource/score.py
+++ src/resource/score.py
@@ -1,4 +1,16 @@
-from flask_restful import Resource
+from flask_restful import Resource, url_for
+from flask import request, Response
+from sqlalchemy.exc import IntegrityError
+from src.utils import ScoreBuilder
+from src.utils import create_error_response
+from src.orm_models import PlayerScore, Game, Player
+from src.extensions import db
+import json
+from jsonschema import validate, ValidationError
+
+MASON = "application/vnd.mason+json"
+NAMESPACE_URL = "/gamescore/link-relations#"
+PROFILE_URL = "/profiles/score/"
 
 class GameScoreboard(Resource):
     def get(self, game_name):
