3257fd8a528337321872a67148dc2f0227293e53
juliushekkala <julius.hekkala@gmail.com>
2020-04-22 18:22:01 +0300

    Small fixes

--- app.py
+++ app.py
@@ -4,4 +4,5 @@ from src.app import create_app
 config_mode = os.getenv('FLASK_ENV') or 'development'
 
 app = create_app(config_mode)
-app.run(host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
